bd65e0aaed502b501442e5843feaa9889eba9795
ksmacf <kenjisato1996@yahoo.co.jp>
2019-08-19 16:10:34 +1000

    Let's see if uploading large files work...

--- test.R
+++ test.R
@@ -2,6 +2,6 @@ library(tidyverse)
 library(dplyr)
 library(xgboost)
 
-test <- read.delim("genLots_data_test.txt", sep = ",")
+test <- read.delim("genLots_data.txt", sep = ",")
 
 test_rmv <- test %>% filter(Muon.PT != "0" | Electron.PT != "0") 
