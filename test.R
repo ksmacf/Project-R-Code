bd1ff2653e1cb15cf37f7a72e5930c0b9d85059b
ksmacf <kenjisato1996@yahoo.co.jp>
2019-08-25 18:47:24 +1000

    Data wrangling made more concise, combined data sets and computing for the best model to build the classifier (most likely Stacked Ensemble).

--- test.R
+++ test.R
@@ -4,8 +4,7 @@ library(dplyr)
 library(caret)
 
 # Load the data of interest
-test <- read.delim(file = "/home/user1/Desktop/Root files/BigData.txt", sep = ",")
-
+test <- read.delim(file = "/home/user1/Desktop/Root files/run_4_data.txt", sep = ",")
 # Remove Electrons and Muons so that we have ONE lepton in each decay
 test_rmv <- test %>% 
   filter(Muon.PT_1 != "0" | Electron.PT_1 != "0") %>% # select non-zero values
@@ -14,11 +13,13 @@ test_rmv <- test %>%
 lepPT_check <- summary(test_rmv$Electron.PT_1==test_rmv$Muon.PT_1) # Check if PT1 for E and M are different at each entry
 
 # Data wrangling with Leptons
-test_rmv$Lepton.PT <- test_rmv$Electron.PT + test_rmv$Muon.PT # Make a new variable to combine results as leptonPT
+test_rmv$Lepton.PT <- test_rmv$Electron.PT_1 + test_rmv$Muon.PT_1 # Make a new variable to combine results as leptonPT
+test_rmv$Lepton.Eta <- test_rmv$Electron.Eta_1 +test_rmv$Muon.Eta_1
+test_rmv$Lepton.Phi <- test_rmv$Electron.Phi_1 + test_rmv$Muon.Phi_1
 test_rmv$Lepton.Type <- as.factor(replace(test_rmv$Electron.PT_1, test_rmv$Electron.PT_1 > 0, "E")) +
   as.factor(replace(test_rmv$Muon.PT_1, test_rmv$Muon.PT_1 > 0, "MU"))
 test_rmv <- select(test_rmv, -"Electron.PT_2", -"Electron.Eta_2", -"Electron.Phi_2", -"Muon.PT_2", -"Muon.Eta_2", -"Muon.Phi_2")
-
+test_rmv$Signal <- length(test_rmv$MissingET.MET) <-1
 # Plotting MET for the full set to do a check
 plot <- ggplot(test, aes(x=MissingET.MET)) +
   geom_histogram(bins = 52) +
