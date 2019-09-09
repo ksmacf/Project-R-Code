8cd63820da18d7130a61704a1f29e490960c4f9f
Kenji Macfarlane <kenjisato1996@yahoo.co.jp>
2019-09-09 17:26:30 +1000

    Delete check_packages.R

--- check_packages.R
+++
@@ -1,8 +0,0 @@
-allPackages <- installed.packages()[, 1]
-findBrokenPackages <- function(packages) {
-  for (p in packages) {
-    tryCatch(ncol(asNamespace(p)$.__NAMESPACE__.$S3methods),
-             error = function(e) print(c(p, e)))
-  }
-}
-findBrokenPackages(allPackages)
